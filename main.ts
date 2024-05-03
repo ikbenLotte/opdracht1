05d63566dd80af6eb17a64b851fcfdff1531eb0c
ikbenLotte <168833609+ikbenLotte@users.noreply.github.com>
2024-05-03 15:06:31 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+})
